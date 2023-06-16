3ed8c1dd877236bbc8fc40b3c394d0a29bff7475
vyhai123 <136839186+vyhai123@users.noreply.github.com>
2023-06-17 00:12:20 +0700

    Gửi bạn Mai
    bla bla bla bla

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'hế lô bạn à',
+    introDesc: `bạn bè hơn chục năm cụng gọi là bạn già rồi hè`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    title: 'If you never had Friends, you never lived life',
+    desc: 'Mày là ai (((: ',
+    btnYes: 'Mai bún lèo :3',
+    btnNo: 'Dương Quỳnh Mai :3',
+    question: 'Đừng chém t sau câu trên hây ((:',
+    btnReply: 'Vì t là Mai béo',
+    reply: 'không nha',
+    mess: 'Tại là văn có hạn nên mần ri cho dài thêm tỉ ((:',
+    messDesc: 'tóm lại chúc bạn đỗ nv1 hây',
+    btnAccept: 'gút bai',
+    messLink: 'https://www.facebook.com/profile.php?id=100014694816776' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
