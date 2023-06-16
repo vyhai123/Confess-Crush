bbcf89900c3b0708c23cdf4aeea7f83b39f1bfe6
vyhai123 <136839186+vyhai123@users.noreply.github.com>
2023-06-17 04:11:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,16 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+const CONFIG = {
+    titleWeb: "Ẩn danh",
+    introTitle: 'Gửi bạn Mai béo',
+    introDesc: `MÃI BÊN NHAU BẠN NHÁ(((:`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
-}
+    title: 'If you never had Friends, you never lived life',
+    desc: 'Bạn là? ',
+    btnYes: 'Mai Bún lèo D:',
+    btnNo: 'Dương Quỳnh Mai D:',
+    question: 'thi xong bao cả bọn đi ăn hây ((:',
+    btnReply: 'gửi cho 7 đứa còn lại',
+    reply: 'tất nhiên',
+    mess: 'Ok chốt nhá',
+    messDesc: 'chúc Mai Đỗ nv1 nha thi xong leo ctuong thôi bonus:https://gifft.me/3d#GgMclAIrTf5b3qx2hkdx',
+    btnAccept: 'Okiiiii lun ',
